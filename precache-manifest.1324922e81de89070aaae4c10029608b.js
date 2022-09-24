13230ebc863667bbf5cec76e34545e0f9a3636ca
alishaiaz <shaiazali@gmail.com>
2022-09-24 16:59:40 +0600

    Updates

---
+++ precache-manifest.1324922e81de89070aaae4c10029608b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2419dfe52feb6f7980f3",
+    "url": "/static/css/main.edaa0230.chunk.css"
+  },
+  {
+    "revision": "2419dfe52feb6f7980f3",
+    "url": "/static/js/main.2419dfe5.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "70c83d2a5799e25e7a22",
+    "url": "/static/js/2.70c83d2a.chunk.js"
+  },
+  {
+    "revision": "7af4db86e54e523836d2",
+    "url": "/static/js/runtime~main.7af4db86.js"
+  },
+  {
+    "revision": "f9ffe7a2f9b99179d795c5713ea35956",
+    "url": "/static/media/facebook.f9ffe7a2.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "b37b05a713d8eebcbb475119e587859b",
+    "url": "/static/media/github.b37b05a7.png"
+  },
+  {
+    "revision": "53a1d45198f240e5d1837b248b05c317",
+    "url": "/static/media/web.53a1d451.png"
+  },
+  {
+    "revision": "c1471e9728a1628662031844c7feb27e",
+    "url": "/static/media/33829.c1471e97.jpg"
+  },
+  {
+    "revision": "eee00944ae957e871e960a81695ead8e",
+    "url": "/index.html"
+  }
+];
