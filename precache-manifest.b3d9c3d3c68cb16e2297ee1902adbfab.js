73e202709b53024e53c78727325bb02bd72cb400
alishaiaz <shaiazali@gmail.com>
2022-09-27 17:22:27 +0600

    Updates

---
+++ precache-manifest.b3d9c3d3c68cb16e2297ee1902adbfab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "abeb0483b794bc2a80c6",
+    "url": "/DEC-Group-7/static/css/main.edaa0230.chunk.css"
+  },
+  {
+    "revision": "abeb0483b794bc2a80c6",
+    "url": "/DEC-Group-7/static/js/main.abeb0483.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/DEC-Group-7/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "0d454d0e4be411b743fe",
+    "url": "/DEC-Group-7/static/js/2.0d454d0e.chunk.js"
+  },
+  {
+    "revision": "399d4aeb39838542664f",
+    "url": "/DEC-Group-7/static/js/runtime~main.399d4aeb.js"
+  },
+  {
+    "revision": "f9ffe7a2f9b99179d795c5713ea35956",
+    "url": "/DEC-Group-7/static/media/facebook.f9ffe7a2.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/DEC-Group-7/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "b37b05a713d8eebcbb475119e587859b",
+    "url": "/DEC-Group-7/static/media/github.b37b05a7.png"
+  },
+  {
+    "revision": "53a1d45198f240e5d1837b248b05c317",
+    "url": "/DEC-Group-7/static/media/web.53a1d451.png"
+  },
+  {
+    "revision": "c1471e9728a1628662031844c7feb27e",
+    "url": "/DEC-Group-7/static/media/33829.c1471e97.jpg"
+  },
+  {
+    "revision": "5eea7a960dda57a400595ef218e67c63",
+    "url": "/DEC-Group-7/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dec/precache-manifest.359ec95cb9f2624d9c243028f16dd06c.js"
+  "/DEC-Group-7/precache-manifest.b3d9c3d3c68cb16e2297ee1902adbfab.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dec/index.html", {
+workbox.routing.registerNavigationRoute("/DEC-Group-7/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
