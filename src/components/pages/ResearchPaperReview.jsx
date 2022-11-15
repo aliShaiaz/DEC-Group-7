a3f70a3090078955ce632caddb4a23633eb04ad5
alishaiaz <shaiazali@gmail.com>
2022-11-15 14:34:37 +0600

    fake RP

--- src/components/pages/ResearchPaperReview.jsx
+++ src/components/pages/ResearchPaperReview.jsx
@@ -5,6 +5,15 @@ const ResearchPaperReview = () => {
             <h1>
                 R Paper Review
             </h1>
+
+
+            <a href='https://downgit.github.io/#/home?url=https://github.com/aliShaiaz/dec/blob/gh-pages/LabReports/rp.pdf'>
+                <button className='btn'>  Research Paper</button>
+            </a>
+            <br />
+            <a href='https://downgit.github.io/#/home?url=https://github.com/aliShaiaz/dec/blob/gh-pages/LabReports/rrp.pdf'>
+                <button className='btn'>  Review of The Research Paper</button>
+            </a>
         </>
     );
 }
--- src/components/pages/labReports.jsx
+++ src/components/pages/labReports.jsx
@@ -10,11 +10,23 @@ const LabReports = () => {
             </div>
             <div className='LRBody'>
                 <a href='https://downgit.github.io/#/home?url=https://github.com/aliShaiaz/dec/blob/gh-pages/LabReports/Lab01.pdf'>
-                    <button className='btn'>Lab Report 01</button>
+                    <button className='btn'>  Determination of Characteristic Curve of a Diode  </button>
                 </a>
                 <br />
                 <a href='https://downgit.github.io/#/home?url=https://github.com/aliShaiaz/dec/blob/gh-pages/LabReports/Lab02.pdf'>
-                    <button className='btn'>Lab Report 02</button>
+                    <button className='btn'>  Study of Diode Rectifiers  </button>
+                </a>
+                <br />
+                <a href='https://downgit.github.io/#/home?url=https://github.com/aliShaiaz/dec/blob/gh-pages/LabReports/Lab04.pdf'>
+                    <button className='btn'>  Study of Zener Diode  </button>
+                </a>
+                <br />
+                <a href='https://downgit.github.io/#/home?url=https://github.com/aliShaiaz/dec/blob/gh-pages/LabReports/Lab05.pdf'>
+                    <button className='btn'>  Study of Transistor Characteristic in Common Emitter Amplifier</button>
+                </a>
+                <br />
+                <a href='https://downgit.github.io/#/home?url=https://github.com/aliShaiaz/dec/blob/gh-pages/LabReports/Lab06.pdf'>
+                    <button className='btn'>  Study of BJT Biasing Circuit</button>
                 </a>
             </div>
         </>
